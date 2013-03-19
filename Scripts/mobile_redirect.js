053c3726d94d622836c5965fb0c21b9b0a07a054
marcushann <marcushann@epikbeats.co.uk>
2013-03-19 19:18:08 +0000

    Update mobile_redirect.js

--- Scripts/mobile_redirect.js
+++ Scripts/mobile_redirect.js
@@ -1,3 +1,7 @@
+//------------------------------------------------------------------------------------------------------
+// This piece of code detects wether the device is IOS and if it is redirects to the ip address
+//------------------------------------------------------------------------------------------------------
+
 function IOS(){
 	//return true or false if device is IOS
 	var IOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
@@ -6,9 +10,18 @@ function IOS(){
 		window.location = "http://87.117.228.65:11728/listen.pls";
 	}
 }
+
+// this calls the function IOS
+
 IOS();
+
+//------------------------------------------------------------------------------------------------------
+// This piece of code detects wether the device is Android and if it is redirects to the mobile site
+//------------------------------------------------------------------------------------------------------
+
 var ua = navigator.userAgent.toLowerCase();
 var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
 if(isAndroid) {
+  // This redirects to the mobile website, to change location change the line below
   window.location = 'mobile%20website/index.html';
 }
